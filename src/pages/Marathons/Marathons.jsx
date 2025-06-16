f3916daf61bd23d0804f0bce0796347e9b615b8f
greenFruits <mdabdullah6548ctg@gmail.com>
2025-06-16 16:57:19 +0600

    added sorting fearture in marathons page

--- src/pages/Marathons/Marathons.jsx
+++ src/pages/Marathons/Marathons.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router";
 import MarathonCard from "../sharedComponents/MarathonCard";
 import Loading from "../../components/Loader/Loading";
+import { MarathonSelectInput } from "../Dashboard/AddMarathon/Inputs";
+import axios from "axios";
 
 function Marathons() {
   const marathons = useLoaderData();
+  const [sortedMarathons, setSortedMarathonns] = useState([]);
+  const [sortedValue, setSortedValue] = useState("");
 
+  const handleSorting = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    setSortedValue(value);
+    axios
+      .get(`http://localhost:3000/sorted-marathons?sortValue=${value}`)
+      .then((res) => {
+        console.log(res.data);
+        res.data.forEach((m) => {
+          console.log(m.createdAt);
+        });
+        setSortedMarathonns(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   if (!marathons) return <Loading />;
   // console.log(marathons);
   return (
@@ -21,11 +42,32 @@ function Marathons() {
           yourself and stay fit. Register early to secure your spot!
         </p>
       </div>
-
+      <div>
+        <fieldset className="fieldset max-w-[220px] flex  bg-base-200 border-base-300 rounded-box w-full border p-4">
+          <label className="label capitalize text-gray-800 dark:text-gray-400 text-base font-medium">
+            sort by
+          </label>
+          <select
+            name="sort"
+            onChange={handleSorting}
+            value={sortedValue}
+            className="select py-1  w-full "
+            required
+          >
+            <option value="default">Default</option>
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
+          </select>
+        </fieldset>
+      </div>
       <div className="grid md:grid-cols-2 xl:grid-cols-3 my-16 gap-x-20 gap-y-10 ">
-        {marathons.map((marathon) => (
-          <MarathonCard key={marathon._id} marathon={marathon} />
-        ))}
+        {sortedMarathons.length
+          ? sortedMarathons.map((marathon) => (
+              <MarathonCard key={marathon._id} marathon={marathon} />
+            ))
+          : marathons.map((marathon) => (
+              <MarathonCard key={marathon._id} marathon={marathon} />
+            ))}
       </div>
     </div>
   );
